698e35c41fd87bb36d9f226f74318e05228c91bd
MrFICAX <79323284+MrFICAX@users.noreply.github.com>
2022-01-15 12:00:57 +0100

    Redefinisanje strukture backEnd-a

--- Application/BackEnd/DonkeyGameBackEnd/Services/Services/CardService.cs
+++ Application/BackEnd/DonkeyGameBackEnd/Services/Services/CardService.cs
@@ -1,11 +1,12 @@
-﻿using Repository;
+﻿using DataAccess.Models;
+using Repository;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataAccess.Models;
+
 
 namespace Services.Services
 {
