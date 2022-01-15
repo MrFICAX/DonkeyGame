cb4eba8d9b9af61957357c8dac2fd60181d6a991
MrFICAX <79323284+MrFICAX@users.noreply.github.com>
2022-01-15 11:53:28 +0100

    CardBackEnd Api added

--- Application/BackEnd/DonkeyGameBackEnd/Services/Services/CardService.cs
+++ Application/BackEnd/DonkeyGameBackEnd/Services/Services/CardService.cs
@@ -1,13 +1,27 @@
-﻿using Services.Interfaces;
+﻿using Repository;
+using Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Models;
 
 namespace Services.Services
 {
     public class CardService : ICardService
     {
+        #region HelpAttributes
+        private const string alphaUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string alphaLowercase = "abcdefghijklmnopqrstuvwxyz";
+        private Random random;
+        #endregion
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public Task<List<Card>> getCards()
+        {
+           return this.unitOfWork.CardRepository.GetAll();
+        }
     }
 }
